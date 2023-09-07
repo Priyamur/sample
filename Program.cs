1f7861b4b015c18c1bf1df705701ccf638218da5
Laevi Jerisha Antony Amal Raj <laevi.antony@itp.objectfrontier.com>
2023-09-07 12:42:09 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public abstract class Hospital{
+    public abstract void doctors();
+    public void numofbeds(){
+        Console.WriteLine("100");
+    }
+
+}
