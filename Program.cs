d4d12d5fe4c87b95bdb00cfc38e9ab995f57471e
Laevi Jerisha Antony Amal Raj <laevi.antony@itp.objectfrontier.com>
2023-09-07 15:34:53 +0530

    se

--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-public abstract class Hospital{
-    public abstract void doctors();
+abstract class Hospital{
+    public abstract void d();
     public void numofbeds(){
-        Console.WriteLine("100");
-    }
+       Console.WriteLine("100");
+  }
 
 }
+class Project : Hospital
+{
+    public override void d(){
+        Console.WriteLine("Doctors: Nithiya");
+        Console.WriteLine("second Doctor Priya");
+    }
+    
+}
+
+ class Program
+ {
+   public static void Main(string[] args){
+    
+   Project Ref=new Project();
+   Ref.d();
+   Ref.numofbeds();
+    }
+}
+
+
